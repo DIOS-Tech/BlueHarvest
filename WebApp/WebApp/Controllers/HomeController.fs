ff74d3286f8f9e48e97b25e608f53a2d5d8a1935
Matt <mwood@dios-tech.com>
2015-02-01 18:55:08 -0500

    Included Fsharp MVC solution into project,

---
+++ WebApp/WebApp/Controllers/HomeController.fs
@@ -0,0 +1,22 @@
+namespace BlueHarvest.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Web
+open System.Web.Mvc
+open System.Web.Mvc.Ajax
+
+open BlueHarvest.Infrastructure.Helpers
+
+type HomeController() =
+    inherit Controller()
+    member this.Index () = this.View()
+    
+    member this.About () =
+      this.ViewData?Message <- "Your application description page."
+      this.View()
+
+    member this.Contact() =
+      this.ViewData?Message <- "Your contact page."
+      this.View()
