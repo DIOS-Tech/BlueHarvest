548d86294b8b70cff95aa743633c6550447f4ded
Frank Hale <frankhale@gmail.com>
2015-02-11 00:33:18 -0500

    Update README / Nuget Packages
    - FYI, updating Nuget packages in F# projects is a pain point! The
    bootstrap update was doing all sorts of stupid shit. I had to manually
    intervene! ARGH!

---
+++ BlueHarvest.NET/Data/DataAccess.fs
@@ -0,0 +1,21 @@
+﻿module DataAccess
+
+open System
+open System.Data
+open System.Data.Linq
+open Microsoft.FSharp.Data.TypeProviders
+open Microsoft.FSharp.Linq
+
+module SQL =
+
+  //FIXME: This is likely not the connection string you'd use on your system, LOL!
+  type dbSchema = SqlDataConnection<"Data Source=.\SQLEXPRESS2014;Initial Catalog=BlueHarvest;Integrated Security=SSPI;">
+  let db = dbSchema.GetDataContext()
+
+  // At this point typing db and hitting . will show you all the tables
+  // now. Type Providers are fucking bad ass! There is really no need to create 
+  // intermediate data models unless one wants to provide other data access 
+  // layers that depend on other types of database systems. 
+  //
+  // I could see the need for exposing generic data models such that one could
+  // swap out back end databases and not have to touch the consumers.
---
+++ BlueHarvest.NET/Data/Models.fs
@@ -0,0 +1,72 @@
+﻿namespace Data
+
+open System
+open System.Collections.Generic
+
+module Models = 
+
+  // Inclusion of these here is debatable. These are either going to be used
+  // as view models or data models.
+
+  //FIXME: These need some serious love! Somebody take a look at these and
+  // make sure they are sane!
+
+  [<CLIMutable>]
+  type Employee = 
+    { UniqueUserName : string
+      DisplayName: string
+      JobTitle: string
+      Team : Team
+      Manager : Employee }
+
+  and Team = 
+    { Name : String
+      Description : String
+      Manager : Employee }
+
+  [<CLIMutable>]
+  type ChangeEntry = 
+    { ChangingEmployee : Employee
+      ChangingTime : DateTime
+      OldValue : String
+      NewValue : String }
+
+  [<CLIMutable>]
+  type AccessEntry = 
+    { Employee : Employee
+      AccessTime : DateTime }
+
+  [<CLIMutable>]
+  type Shift = 
+    { StartTime : DateTime
+      EndTime : DateTime
+      Day : int
+      Employee : Employee }
+
+  [<CLIMutable>]
+  type Request = 
+    { Employee : Employee
+      StartDate : DateTime 
+      EndDate : DateTime
+      Notes : string }
+
+  [<CLIMutable>]
+  type RequestEntry = 
+    { ChangingEmployee : Employee
+      ChangingTime : DateTime
+      Request : Request }
+
+  // need to enforce phone numbers here
+  // need to build an enum to handle the phoneType
+  [<CLIMutable>]
+  type PhoneNumber = 
+    { PhoneNumber : string
+      PhoneType : int
+      Employee : Employee }
+
+  [<CLIMutable>]
+  type OnCallEntry = 
+    { Employee : Employee
+      ShiftStart : DateTime
+      ShiftFinish : DateTime }
+
